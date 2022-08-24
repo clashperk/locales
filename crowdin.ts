5dca974513d5c3e52f201ca06ce99c269cdcb8dc
Suvajit <suvajit.me@gmail.com>
2022-08-24 22:47:31 +0530

    switch to undici fetch api

--- crowdin.ts
+++ crowdin.ts
@@ -1,7 +1,5 @@
 import 'reflect-metadata';
-import { request as fetch, Dispatcher } from 'undici';
-
-const isOk = (res: Dispatcher.ResponseData) => res.statusCode >= 200 && res.statusCode < 300;
+import { fetch } from 'undici';
 
 (async () => {
     const response = await fetch('https://api.crowdin.com/api/v2/projects/522390/strings?fileId=18&limit=500', {
@@ -11,10 +9,10 @@ const isOk = (res: Dispatcher.ResponseData) => res.statusCode >= 200 && res.stat
         },
         method: 'GET'
     });
-    const result = await response.body.json();
-    if (response.statusCode !== 200) console.log(JSON.stringify(result));
+    const result = await response.json();
+    if (!response.ok) console.log(JSON.stringify(result));
 
-    for (const item of result.data ?? []) {
+    for (const item of (result as any).data ?? []) {
         if (item.data.identifier.endsWith('.description') && item.data.maxLength === 0) {
             if (item.data.text.length > 100) {
                 console.log(`${item.data.identifier as string} is too long.`);
@@ -34,8 +32,8 @@ const isOk = (res: Dispatcher.ResponseData) => res.statusCode >= 200 && res.stat
                 ]),
                 method: 'PATCH'
             });
-            const data = await res.body.json();
-            if (!isOk(res)) console.log(JSON.stringify(data));
+            const data = await res.json();
+            if (!res.ok) console.log(JSON.stringify(data));
             console.log(`${item.data.identifier as string} updated.`);
         }
     }
--- index.ts
+++ index.ts
@@ -1,13 +1,21 @@
-import 'reflect-metadata';
+import { InitOptions } from 'i18next';
 
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-import locale from './en.js';
+export const fallbackLng = {
+    'fr': ['fr-FR', 'en-US'], // French/Français
+    'it': ['it-IT', 'en-US'], // Italian/Italiano
+    'de': ['de-DE', 'en-US'], // German/Deutsch
+    'no': ['no-NO', 'en-US'], // Norwegian/Norsk
+    'nl': ['nl-NL', 'en-US'], // Dutch/Nederlands
+    'es-ES': ['es-ES', 'en-US'], // Spanish/Español
+    'pt-BR': ['pt-BR', 'en-US'] // Portuguese/Português
+};
 
-(() => {
-    {
-        const data = JSON.stringify(locale, null, 2);
-        fs.writeFileSync(fileURLToPath(new URL('../../locales/en-US/translation.json', import.meta.url)), data);
-        fs.writeFileSync(fileURLToPath(new URL('../locales/en-US/translation.json', import.meta.url)), data);
-    }
-})();
+export const defaultOptions: InitOptions = {
+    debug: false,
+    cleanCode: true,
+    lng: 'en-US',
+    defaultNS: 'translation',
+    ns: ['translation'],
+    fallbackLng: { ...fallbackLng, default: ['en-US'] },
+    preload: ['en-US', 'en-GB', 'es-ES', 'fr-FR', 'nl-NL', 'it-IT', 'de-DE', 'no-NO', 'pt-BR']
+};
