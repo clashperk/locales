aa214c3209210bda32709defa0b20f35d875eb45
suvajit <suvajit.me@gmail.com>
2024-05-01 14:01:34 +0530

    chore(eslint): switch to 2 space indent

--- export.ts
+++ export.ts
@@ -5,7 +5,7 @@ import fs from 'fs';
 import locale from './en.js';
 
 (() => {
-    const data = JSON.stringify(locale, null, 2);
-    fs.writeFileSync(fileURLToPath(new URL('../../locales/en-US/translation.json', import.meta.url)), data);
-    fs.writeFileSync(fileURLToPath(new URL('../locales/en-US/translation.json', import.meta.url)), data);
+  const data = JSON.stringify(locale, null, 2);
+  fs.writeFileSync(fileURLToPath(new URL('../../locales/en-US/translation.json', import.meta.url)), data);
+  fs.writeFileSync(fileURLToPath(new URL('../locales/en-US/translation.json', import.meta.url)), data);
 })();
